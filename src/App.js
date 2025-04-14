92447b13590e9e2b4c93f658b60a526b70aacc3f
Your Name <you@example.com>
2025-04-14 10:09:39 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './components/Hero'
+import PracticeHook from './components/PracticeHook'
 
 const App = () => {
   return (
     <div>
-      <Hero/>
+      <Hero />
+      <PracticeHook/>
     </div>
   )
 }
